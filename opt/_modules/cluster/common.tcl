9898ff199e5d1471af05ae879fcc177d7a887146
Hurng-Chun Lee <hurngchunlee@users.noreply.github.com>
2026-07-21 15:31:14 +0200

    Update common.tcl
    removed PATH settings for removed external packages

--- opt/_modules/cluster/common.tcl
+++ opt/_modules/cluster/common.tcl
@@ -21,14 +21,6 @@ if { $arch == "linux_x86_64" } {
 }
 append-path MANPATH "$env(CLUSTER_UTIL_ROOT)/external/utilities/man"
 
-# add external tools included as part of the cluster package
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/project_acl"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/keepassx/latest/bin"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/plink"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/gcta"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/ldsc/ldsc"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/magma"
-
 # add p7zip tool
 prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/p7zip-16.02/bin"
 prepend-path MANPATH "$env(CLUSTER_UTIL_ROOT)/external/p7zip-16.02/man"
@@ -42,19 +34,3 @@ prepend-path MANPATH "$env(CLUSTER_UTIL_ROOT)/external/lzip/share/man"
 prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/pigz/bin"
 prepend-path MANPATH "$env(CLUSTER_UTIL_ROOT)/external/pigz/man"
 
-# add the path for torque and maui/moab
-#if { $osversion == "7" } {
-if { [regexp {^7.*} [get_redhat_version]] } {
-    setenv TORQUEHOME /var/spool/torque
-}
-
-#setenv MAUIHOMEDIR /usr/local/maui
-#append-path LD_LIBRARY_PATH $env(MAUIHOMEDIR)/lib
-#append-path PATH $env(MAUIHOMEDIR)/bin
-#append-path PATH $env(MAUIHOMEDIR)/sbin
-
-setenv MOABHOMEDIR "$env(CLUSTER_UTIL_ROOT)/external/moab"
-prepend-path PATH "$env(CLUSTER_UTIL_ROOT)/external/moab/bin"
-prepend-path MANPATH "$env(CLUSTER_UTIL_ROOT)/external/moab/share/man"
-
-set-alias boxes "boxes -f $env(CLUSTER_UTIL_ROOT)/etc/boxes-config"
